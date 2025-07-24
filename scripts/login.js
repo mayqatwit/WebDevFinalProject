0bc1fb247b8eb1b0479d3353a4c39ab1c28797e7
mayqatwit <mayq@wit.edu>
2025-07-24 10:32:04 -0400

    Started adding mobile implementation for login screen

--- scripts/login.js
+++ scripts/login.js
@@ -1,11 +1,21 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const signUpMobile = document.getElementById('signUpMobile');
+    const signInMobile = document.getElementById('signInMobile');
+    const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
+    signUpButton?.addEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+    signInButton?.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
+
+    signUpMobile?.addEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
+
+    signInMobile?.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
